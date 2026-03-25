7e7ca1893c84ec43d1a8a8ae5473fd1aecbd4fae
nayelimancia19-coder <nayelimancia19@gmail.com>
2026-03-24 23:04:06 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "graphito.h"
 #include "Canon.h"
+#include "Botones.h"
 
 using namespace graphito;
 
@@ -7,9 +8,25 @@ int main(){
 
     VDefine(900,600,"Bala de Canon");
 
+
     Canon c1(70,450,110);
 
     c1.mostrar();
+
+     Boton br(280,500,100,50,EBT_3D,"Fuego");
+     Boton ba(420,500,100,50,EBT_3D,"Fin");
+     Boton bv(550,500,100,50,EBT_3D,"+");
+     Boton bs(700,500,100,50,EBT_3D,"-");
+     Espera(100);
+     br.mostrar();
+     ba.mostrar();
+     bv.mostrar();
+     bs.mostrar();
+
+        if(br.click()){
+            Mensaje("hola");
+        }
+
 
     int tecla = TC_NINGUNA;
     int angulo = 35;
